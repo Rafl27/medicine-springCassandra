5b78fa98b7edeadee036ef0fa5cac50a93cefdb2
Glenda Faria dos Santos <glenda.faria@dti.com.br>
2023-05-16 19:18:01 -0300

    cassandra implementation

---
+++ src/main/kotlin/com/distribuido/simpleCrud/config/AppConfig.java
@@ -0,0 +1,13 @@
+//package com.distribuido.simpleCrud.config;
+//
+//import com.datastax.oss.driver.api.core.CqlSession;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//
+//@Configuration
+//public class AppConfig {
+//    public @Bean CqlSession cassandraSession() {
+//        return CqlSession.builder().withKeyspace("remedio").build();
+//    }
+//}
